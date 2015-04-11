4aacd996c3ce488f30c0db097c012320b8633ff6
Ricardo <ricardo.b.silva13@gmail.com>
2015-04-11 18:46:50 -0300

    adicionei formulario

--- view/index.php
+++ view/index.php
@@ -2,8 +2,17 @@
 
     <container>
         <div class="row">
-            <div class="col-xs-12 col-md-8">.col-xs-12 .col-md-8</div>
+<!--            <div class="col-xs-12 col-md-8">.col-xs-12 .col-md-8</div>
             <div class="col-xs-6 col-md-4">.col-xs-6 .col-md-4</div>
-            <div class="col-xs-6 col-md-4">.col-xs-6 .col-md-4</div>
+            <div class="col-xs-6 col-md-4 ">.col-xs-6 .col-md-4</div>-->
+            
+            <div class="col-md-4 col-md-offset-4 " >
+                <div class="panel panel-default">
+                    <div class="panel-heading">Panel heading without title</div>
+                    <div class="panel-body">
+                        <input type="text" class="form-control"  >
+                    </div>
+                </div>
+            </div>
         </div>
     </container>
