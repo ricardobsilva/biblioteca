3af00a7437ddbbd0c8c527502f48d2124ac26052
Ricardo <ricardo.b.silva13@gmail.com>
2015-04-26 13:17:11 -0300

    listagem de usuários finalizada

--- conexao/Conexao.php
+++ conexao/Conexao.php
@@ -11,7 +11,7 @@ class Conexao{
 		if(!isset(self::$instance)){
 
 			try {
-				self::$instance = new PDO('mysql:host=' . DB_HOST . ';dbname=' . DB_NAME, DB_USER, DB_PASS);
+				self::$instance = new PDO('mysql:host=' . DB_HOST . ';dbname=' . DB_NAME, DB_USER, DB_PASS , array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
 				self::$instance->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 				self::$instance->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
 			} catch (PDOException $e) {
--- view/Usuario/listar.php
+++ view/Usuario/listar.php
@@ -1,12 +1,73 @@
+<link href="../../css/bootstrap.min.css" rel="stylesheet">
+
 <?php
 include '../../control/UsuarioController.php';
+include '../../template/menu.php';
 $usuarioController = new UsuarioController();
-
-foreach ($usuarioController->findAll() as $usuario) {
-    echo $usuario->descricao;
-    echo $usuario->email;
-    echo $usuario->status;
-}
-
-
 ?>
+
+<div class="col-md-10 col-md-offset-1">
+    <div class="panel panel-primary">
+        <!-- Default panel contents -->
+        <div class="panel-heading">usuários cadastrados</div>
+        <div class="panel-body">
+
+            <button type="button" class="btn btn-primary btn-sm col-md-offset-11" aria-label="Left Align">
+                <span class="glyphicon glyphicon-plus" aria-hidden="true"></span>
+                <a href="http://www.globo.com" target="_blank" style="color: #ffffff; text-decoration: none"  >adicionar</a>
+            </button><br><br>    
+
+            <table class="table table-striped table-hover">
+                <tr>
+                    <th>
+                        Id
+                    </th>
+                    <th>
+                        Nome
+                    </th>
+                    <th>
+                        Email
+                    </th>
+                    <th>
+                        Status
+                    </th>
+                    <th>
+                        Ações
+                    </th>
+                </tr>
+                <?php foreach ($usuarioController->findAll() as $usuario) { ?>
+                    <tr>   
+                        <td><?php echo $usuario->id; ?></td>
+                        <td><?php echo $usuario->descricao; ?></td>
+                        <td><?php echo $usuario->email; ?></td>
+                        <td>
+                            <?php if ($usuario->status == "ATIVO") {
+                                ?>
+                                <span class="label label-success"><?php echo $usuario->status; ?></span>
+                            <?php } else { ?>        
+                                <span class="label label-danger"><?php echo $usuario->status; ?></span>
+                            <?php } ?>
+                        </td>    
+                        <td>
+                            <div class="dropdown">
+                                <button class="btn btn-info dropdown-toggle btn-xs" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-expanded="true">
+                                    ações
+                                    <span class="caret"></span>
+                                </button>
+                                <ul class="dropdown-menu" role="menu" aria-labelledby="dropdownMenu1">
+                                    <li role="presentation"><a role="menuitem" tabindex="-1" href="#">editar</a></li>
+                                    <li role="presentation"><a role="menuitem" tabindex="-1" href="#">excluir</a></li>
+                                </ul>
+                            </div>  
+                        </td>
+                    </tr>
+                <?php } ?>
+            </table>
+        </div>
+    </div>
+</div>    
+
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+<!-- Include all compiled plugins (below), or include individual files as needed -->
+<script src="../../js/bootstrap.min.js"></script>
